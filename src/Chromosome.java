6d4aeeb2b195541f4c545f1fe86b5b6e4ad45b39
Jakub Kornacki <jakubkornacki358@gmail.com>
2023-02-04 01:10:53 +0000

    more work on the algorithm fitness function left to implemented

--- src/Chromosome.java
+++ src/Chromosome.java
@@ -1,106 +1,128 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Chromosome {
-    private StringBuilder genes;
-    private StringBuilder chromosome;
+    private int[] genes;
+    private int[] chromosome;
+    private int[] capacity;
     private int fitness;
-
-    private int supervisorId;
-
-    private int capacity;
-
-    private String supervisorName;
-
     private int chromosomeLength;
 
     private int noOfSupervisors;
 
-    public Chromosome(String supervisorName, int capacity, int chromosomeLength, int noOfSupervisors) {
+    public Chromosome(int[] chromosome, int[] capacity, int noOfSupervisors) {
         this.capacity = capacity;
-        this.chromosomeLength = chromosomeLength;
-        this.supervisorId = extractId(supervisorName);
-        this.chromosome = new StringBuilder(chromosomeLength);
-        this.genes = generateGenes(noOfSupervisors);
-        this.noOfSupervisors = noOfSupervisors;
-    }
-
-
-    public Chromosome(String supervisorName, StringBuilder chromosome, int capacity, int noOfSupervisors) {
-        this.supervisorName = supervisorName;
-        this.capacity = capacity;
-        this.chromosomeLength = chromosome.length();
-        this.supervisorId = extractId(supervisorName);
+        this.chromosomeLength = chromosome.length;
         this.chromosome = chromosome;
         this.genes = generateGenes(noOfSupervisors);
         this.noOfSupervisors = noOfSupervisors;
     }
 
-    public StringBuilder generateGenes(int noOfSupervisors) {
-        StringBuilder temp = new StringBuilder();
-        for(int i = 1; i < noOfSupervisors; i++) {
-            temp.append(i-1);
+    public Chromosome(int chromosomeLength, int noOfSupervisors) {
+        this.chromosomeLength = chromosomeLength;
+        this.chromosome = new int[chromosomeLength];
+        this.genes = generateGenes(noOfSupervisors);
+        this.noOfSupervisors = noOfSupervisors;
+    }
+
+    public void setCapacity(int[] capacity) {
+        this.capacity = capacity;
+    }
+
+    public int[] generateGenes(int noOfSupervisors) {
+        int[] temp = new int[noOfSupervisors];
+        for(int i = 0; i < noOfSupervisors; i++) {
+            temp[i] = i+1;
         }
         return temp;
     }
 
 
-    private int extractId(String supervisorName) {
-        String[] parts = supervisorName.split("_");
-        return Integer.parseInt(parts[1]);
+   public void calculateFitness(ArrayList<Student> students) {
+        fitness = 0;
+        // For each student s assigned to work with lecturer l, consider the preference p that the student had
+        // for that lecturer. Sum these preferences together. The lower the sum, the better the solution.
+        int studentsConsidered = 0;
+        for(Student student : students) {
+            int [] preferences = student.getPreferenceList();
+            for (int studentIndex = 0; studentIndex < chromosome.length; studentIndex++) {
+                // if the lecturer is equal
+                if (chromosome[studentIndex] == student.getStudentId()) {
+                    int lecturerIndex = chromosome[studentIndex]-1;
+                    if(studentIndex == 0) {
+                        fitness += preferences[chromosome[studentIndex] -1];
+                    } else {
+                        fitness += preferences[chromosome[studentIndex - 1] - 1];
+                    }
+                    studentsConsidered++;
+                }
+            }
+            if(studentsConsidered > capa)
+
+                studentsConsidered = 0;
+        }
     }
 
-    public void calculateFitness(ArrayList<Student> students) {
-
-
-    }
-
-
-    public StringBuilder getChromosome() {
+    public int[] getChromosome() {
         return chromosome;
+    }
+
+    public int[] getCapacities() {
+        return capacity;
     }
 
 
     // generate the chromosome by randomly appending genes to it
     public void generateRandomChromosome() {
         for(int i = 0; i < chromosomeLength; i++) {
-            int randIndex = (int) (Math.random() * genes.length());
-            chromosome.append(genes.charAt(randIndex));
+            int randIndex = new Random().nextInt(genes.length);
+            chromosome[i] = genes[randIndex];
         }
     }
 
 
     // create a new chromosome from combining parts of two different chromosomes, split position of the chromosomes is the crossover point
     public static Chromosome crossover(Chromosome ch1, Chromosome ch2, int crossoverPoint) {
-        String temp = ch1.getChromosome().substring(0, crossoverPoint);
-        String temp2 = ch2.getChromosome().substring(crossoverPoint, ch2.getChromosome().length());
-        return new Chromosome(new String(ch1.supervisorName), new StringBuilder(temp + temp2), ch1.capacity, ch1.noOfSupervisors);
+        int[] newChromosome = new int[ch1.chromosomeLength];
+        for(int i = 0; i < ch1.chromosomeLength; i++) {
+            if(i  < crossoverPoint) {
+                newChromosome[i] = ch1.getChromosome()[i];  // int array
+            } else {
+                newChromosome[i] = ch2.getChromosome()[i];
+            }
+        }
+        return new Chromosome( newChromosome, ch1.capacity, ch1.noOfSupervisors);
     }
 
 
 
-    public static Chromosome mutate(Chromosome ch1, StringBuilder genes) {
-        // select a random mutation point
-        int mutationPoint = (int) (Math.random() * ch1.getChromosome().length());
-        // remove the gene at mutation point from the list of possible genes (to avoid mutating the gene into the same gene)
-        String toRemove = ch1.getChromosome().substring(mutationPoint, mutationPoint+1);
-        StringBuilder localGenes = new StringBuilder(genes.toString());
-        localGenes.deleteCharAt(localGenes.indexOf(toRemove));
-
-        // select a random gene from the rest of possible genes
-        int randIndex = (int) (Math.random() * genes.length()-1);
-
-        // replace the gene a mutation point with a random gene and return the newly created chromosome
-        StringBuilder newChromosome = new StringBuilder(ch1.toString());
-        newChromosome.setCharAt(mutationPoint, localGenes.charAt(randIndex));
-        return new Chromosome(new String(ch1.supervisorName), newChromosome, ch1.capacity, ch1.noOfSupervisors);
+    public static Chromosome mutate(Chromosome ch1, float mutationRate) {
+        int[] newChromosome = new int[ch1.chromosomeLength];
+        for(int i = 0; i < ch1.chromosomeLength; i++) {
+            newChromosome[i] = ch1.getChromosome()[i];
+            if(Math.random() <= mutationRate) {
+                int randIndex = new Random().nextInt(ch1.chromosomeLength);
+                newChromosome[i] = ch1.getChromosome()[randIndex];
+            }
+        }
+        return new Chromosome(newChromosome, ch1.capacity, ch1.noOfSupervisors);
     }
 
     public int getFitness() {
         return fitness;
     }
 
-    public StringBuilder getGenes(){
+    public int[] getGenes(){
         return genes;
+    }
+
+    public String chromosomeToString(){
+        StringBuilder temp = new StringBuilder();
+        for(int i = 0; i < chromosomeLength; i++) {
+            temp.append("[" + chromosome[i] + "]");
+        }
+        return temp.toString();
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -13,15 +13,19 @@ public class Main {
 
         // parse population size and throw NumberFormatException if contains chars other than numbers
         int generations = Integer.parseInt(args[0]);
-        float crossoverRate = Float.parseFloat(args[1]);
-        float mutationRate = Float.parseFloat(args[2]);
-        int populationSize = Integer.parseInt(args[3]);
+        int populationSize = Integer.parseInt(args[1]);
+        float crossoverRate = Float.parseFloat(args[2]);
+        float mutationRate = Float.parseFloat(args[3]);
 
         ArrayList<Chromosome> supervisors = new ArrayList<Chromosome>();
         ArrayList<Student> students = new ArrayList<Student>();
 
         String line = "";
         String delimiter = ";";
+        String[] data;
+        String studentName;
+        String[] studentPreferences = new String[0];
+        int[] capacitiesOfSupervisors = new int[0];
 
         try
         {
@@ -30,26 +34,24 @@ public class Main {
             BufferedReader br = new BufferedReader(new FileReader("Student-choices.csv"));
             while ((line = br.readLine()) != null)
             {
-                String[] data = line.split(delimiter);
-                String studentName = data[0];
-                String[] studentPreferences = new String[data.length-1];
+                data = line.split(delimiter);
+                studentName = data[0];
+                studentPreferences = new String[data.length-1];
                 for(int i = 0; i < data.length-1; i ++) {
                     studentPreferences[i] = data[i+1];
                 }
                 students.add(Student.createStudent(studentName, studentPreferences));
             }
-
+            capacitiesOfSupervisors = new int[studentPreferences.length];
             // read lecturer data from csv file
             BufferedReader br2 = new BufferedReader(new FileReader("Supervisors.csv"));
             int supervisorIndex = 0;
             while ((line = br2.readLine()) != null)
             {
-                String[] data = line.split(delimiter);
-                String lecturerName = data[0];
-                int lecturerCapacity = Integer.parseInt(data[1]);
-                supervisors.add(new Chromosome(lecturerName,lecturerCapacity, students.size(), supervisors.size()));
+                data = line.split(delimiter);
+                capacitiesOfSupervisors[supervisorIndex] = Integer.parseInt(data[1]);
+                supervisors.add(new Chromosome(students.size(), studentPreferences.length));
                 supervisors.get(supervisorIndex).generateRandomChromosome();
-                supervisors.get(supervisorIndex).calculateFitness(students);
                 supervisorIndex++;
             }
         }
@@ -58,8 +60,9 @@ public class Main {
             e.printStackTrace();
         }
 
-
-
+        for(int i = 0; i < supervisors.size(); i++) {
+            supervisors.get(i).setCapacity(capacitiesOfSupervisors);
+        }
         int chromosomeLength = students.size();
 
         GeneticAlgorithm ga = GeneticAlgorithm.createGA(populationSize, chromosomeLength, generations, crossoverRate, mutationRate, supervisors, students);
